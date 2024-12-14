9f87b51df0dfe97cac0e314ced65f8200fe1d43a
Easter1995 <1073208733@qq.com>
2024-12-14 22:17:22 +0800

    实现翻页

--- WebScraper/spiders/lianjia.py
+++ WebScraper/spiders/lianjia.py
@@ -2,8 +2,8 @@ import scrapy
 import re
 from WebScraper.items import RentHouseItem
 from scrapy_selenium import SeleniumRequest
-from selenium.webdriver.common.by import By
-
+import time
+import random
 
 class LianjiaSpider(scrapy.Spider):
     name = "lianjia"
@@ -11,25 +11,23 @@ class LianjiaSpider(scrapy.Spider):
     city_names = ['dali']
     # city_names = ['bj', 'sh', 'gz', 'sz', 'dali']
     cookies = {
-        "lianjia_uuid": "f4e3d625-f9ef-4796-9fbf-1005748236ee",
-        "Hm_lvt_46bf127ac9b856df503ec2dbf942b67e": "1731846490",
-        "HMACCOUNT": "D904BD81485BDC57",
-        "_jzqc": "1",
-        "_ga": "GA1.2.511800293.1731846504",
-        "sensorsdata2015jssdkcross": "%7B%22distinct_id%22%3A%221933a18487e10e5-05564b0cdb79cf-26011951-1327104-1933a18487f1196%22%2C%22%24device_id%22%3A%221933a18487e10e5-05564b0cdb79cf-26011951-1327104-1933a18487f1196%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%7D",
-        "ftkrc_": "0248dd95-2625-40b5-8dbe-c03e4c9d833a",
-        "lfrc_": "3af73b7d-5de7-4cd3-bdda-fd2781075775",
-        "_ga_RCTBRFLNVS": "GS1.2.1732591395.14.1.1732591402.0.0.0",
-        "_jzqx": "1.1731846496.1732618294.6.jzqsr=bj%2Elianjia%2Ecom|jzqct=/.jzqsr=bj%2Elianjia%2Ecom|jzqct=/ershoufang/",
-        "_ga_KJTRWRHDL1": "GS1.2.1732618303.9.0.1732618303.0.0.0",
-        "_ga_QJN1VP0CMS": "GS1.2.1732618303.9.0.1732618303.0.0.0",
-        "Hm_lpvt_46bf127ac9b856df503ec2dbf942b67e": "1732627892",
-        "GUARANTEE_BANNER_SHOW": "true",
-        "lianjia_ssid": "7ecace34-3b67-40bd-b76e-5f4f5812ec36",
+        "lianjia_uuid": "33bb9625-0e1b-40ac-8a0f-884b7c574e94",
+        "Hm_lvt_46bf127ac9b856df503ec2dbf942b67e": "1732357750,1732608637,1732627932,1734012811",
+        "_jzqa": "1.3216189913405561000.1732110614.1732627931.1734013518.4",
+        "sensorsdata2015jssdkcross": "%7B%22distinct_id%22%3A%2219349d67f0566b-03ccf1f445401c-e525627-1327104-19349d67f06d42%22%2C%22%24device_id%22%3A%2219349d67f0566b-03ccf1f445401c-e525627-1327104-19349d67f06d42%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%7D",
+        "ftkrc_": "6d97c3ac-6236-4934-9c6e-3a8789d47a13",
+        "lfrc_": "ddf035a8-9a2e-4cfd-8a63-c6b8dd091865",
+        "_ga": "GA1.2.1928467867.1732110659",
+        "_ga_QJN1VP0CMS": "GS1.2.1734013530.3.0.1734013530.0.0.0",
+        "_ga_RCTBRFLNVS": "GS1.2.1732357751.2.0.1732357751.0.0.0",
+        "_ga_KJTRWRHDL1": "GS1.2.1734013530.4.0.1734013530.0.0.0",
+        "login_ucid": "2000000455074951",
+        "lianjia_token": "2.0012803a70453ab31b032d134151f3baf8",
+        "lianjia_token_secure": "2.0012803a70453ab31b032d134151f3baf8",
+        "security_ticket": "EUtjTccMx5nei42GfXNMtiAZuyZg66PYjxCd99yBl203g6oQB6ZLtVDRO+mOTmVhn17L9on4JzzWuDlZ0KwPBlIJv0TIW0zs3frUCoIxO/AEoCJu99OLlFAJ305bLnFBuhedNr9Yym8VkspxLWqm2vTe/qbDAu8qyQD9kylBxHo=",
         "select_city": "532900",
-        "srcid": "eyJ0Ijoie1wiZGF0YVwiOlwiOTdlZjRkNzQ4OGY2ZWY2NWRmZWFmYTkyODQ0NWI2MjNiYTc4MDAxN2NkMDc2NzQyNmVlY2NjNDAzMThjZThmMGMyMjJmOTg5MGIzZDZlNmM3M2JjNzgzYjk5MWUxYWQxYWQxOTJmN2YzYzhiODBiODI2ZDI0YTllOTE2ZTAxNGY4NGUxMjRmNmFkMTllMWMxYWI3NDhhNTc1YjhiNDA0MzM4M2QwMjAxNDIzMzM0ODJlNDQ4NmI0MTRhMmY5NTJmZTM3ODYyMDI0ZTBjMzY5MjE1MmU0MzNmMjI4YTkzNmI3YmJjNjk3OTJlNzdmZmQwZmVlNzBkMDJhZTBjOGM4NFwiLFwia2V5X2lkXCI6XCIxXCIsXCJzaWduXCI6XCJhZjFiYTkyMlwifSIsInIiOiJodHRwczovL2RhbGkubGlhbmppYS5jb20venVmYW5nLyIsIm9zIjoid2ViIiwidiI6IjAuMSJ9",
-        "Hm_lpvt_46bf127ac9b856df503ec2dbf942b67e": "1732630462",
-        "_jzqb": "1.17.10.1732624451.1"
+        "beikeBaseData": "%7B%22parentSceneId%22%3A%222021652502261744129%22%7D",
+        "lianjia_ssid": "7a74d013-1429-4e76-a0e9-acab6b356448"
     }
 
     def get_start_urls(self):
@@ -58,13 +56,16 @@ class LianjiaSpider(scrapy.Spider):
         for area in area_href_list:
             area_str = area.xpath('./@href').get().strip().split('/')[2]
             area_url = f"https://{city_name}.lianjia.com/zufang/{area_str}/"
-            urls.append(area_url)
             # 每个街道的起始url
             tmp_url = f"{area_url}pg1"
-            urls.append(tmp_url)
+            urls.append((tmp_url, area_str))
 
         for url in urls[1:]:
-            yield SeleniumRequest(url=url, cookies=self.cookies, callback=self.parse, meta={'city_name': city_name,})
+            yield SeleniumRequest(url=url[0], cookies=self.cookies, callback=self.parse, meta={
+                'city_name': city_name,
+                'area_str': url[1],
+                'page': 1,
+            })
 
     def start_requests(self):    
         for name in self.city_names:
@@ -74,6 +75,8 @@ class LianjiaSpider(scrapy.Spider):
     def parse(self, response):
         page_empty = bool(response.xpath('//div[@class="content__empty1"]'))  # 超出页数范围,会有这个标签
         city_name = response.meta['city_name']  # 从 URL 中提取城市名称
+        area = response.meta['area_str']
+        page = response.meta['page']
         if not page_empty:
             content_list = response.xpath('//*[@id="content"]/div[1]/div[1]/div')
             for content in content_list:
@@ -101,3 +104,16 @@ class LianjiaSpider(scrapy.Spider):
                 lay = re.search(r'<i>/</i>\n(.*)<span class="hide">', des).group(1)
                 house['layout'] = lay.strip()
                 yield house
+        
+            # 随机休眠1-2秒
+            delay = random.uniform(1, 2)
+            time.sleep(delay)
+
+            if page != "pg100":
+                next_page = int(page) + 1
+                next_url = f"https://{city_name}.lianjia.com/zufang/{area}/pg{next_page}/"
+                yield SeleniumRequest(url=next_url, cookies=self.cookies, callback=self.parse, meta={
+                    'city_name': city_name,
+                    'area_str': area,
+                    'page': next_page,
+                })
